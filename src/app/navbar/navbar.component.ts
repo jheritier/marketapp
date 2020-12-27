b5448b389fcd7ebca0dd9eaff5cea60c106f44c2
jerome <jerome.heritier@gmail.com>
2020-12-27 20:08:59 +0100

    v0.0.0

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   hideContent : boolean = false;
+  cartquantity : number = 0;
 
   constructor() { }
 
